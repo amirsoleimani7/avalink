9ca1e2b22f828a7a5d1c4fe2fc36f62dddf47560
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-15 16:54:03 +0330

    remvoe : deplay function on the main page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,10 +4,10 @@ export default function Loading() {
   return (
     <div className="flex w-full h-full items-center justify-center bg-black *:text-white">
       <div className="text-white font-semibold flex gap-1 text-center justify-center">
-        <h1 className="font-bold font-bungee text-6xl max-md:text-5xl max-sm text-4xl text-white">
+        <h1 className="font-bold font-bungee text-6xl max-md:text-5xl max-sm  text-white">
           ava Link
         </h1>
-        <Spinner className="size-3"/>
+        <Spinner className="size-3" />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
 import TextType from "@/components/custom/typewriter";
 
 export default async function Home() {
-  await new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("something");
-    }, 10000);
-  });
+  // await new Promise((resolve, reject) => {
+  //   setTimeout(() => {
+  //     resolve("something");
+  //   }, 10000);
+  // });
 
-  return (
-    <div className="w-full h-full bg-gray-900">
-      {" "}
-      <TextType
-        key={"typewriter"}
-        className="text-white font-bungee text-8xl select-none"
-        text={["AVA LINK"]}
-        typingSpeed={75}
-        pauseDuration={400}
-        showCursor
-        loop
-        cursorCharacter="|"
-      />
-    </div>
-  );
+  return <div className="w-full h-full bg-gray-900"></div>;
 }
