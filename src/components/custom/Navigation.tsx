7a1ed34cbbb5a11d2a925fb3ce6edfd4a38dc7bc
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-16 01:22:19 +0330

    add : marigns fixed

--- src/components/custom/Navigation.tsx
+++ src/components/custom/Navigation.tsx
@@ -11,12 +11,12 @@ import { MenuData, MenuType, MenuLink } from "@/data/MenuItems";
 
 export default function Navigation() {
   return (
-    <div className="fixed  w-full bg-black border-b border-gray-800 text-white flex items py-4  px-[15%] justify-between select-none">
+    <div className="fixed  w-full bg-black border-b border-gray-800 text-white flex items py-4  px-[15%] max-xl:px-[10%] max-md:px-[8%] max-sm:px-[4%] justify-between select-none">
       <span className="font-bungee text-3xl duration-200 transition-all ease-in-out hover:text-gray-300 cursor-pointer">
         AVA LINK
       </span>
-
-      <div className="">
+      
+      <div className="max-lg:hidden">
         <NavigationMenu className={""}>
           <NavigationMenuList>
             {MenuData.map((menuItem: MenuType, index: number) => (
