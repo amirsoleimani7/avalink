b132bf3377c217b510de03e261036525a16b9946
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-16 11:18:33 +0330

    add : data to sidebar menu

--- src/components/custom/Navigation.tsx
+++ src/components/custom/Navigation.tsx
@@ -12,7 +12,7 @@ import SideBarNavigation from "./SideBarNavigation";
  
 export default function Navigation() {
   return (
-    <div className="fixed  w-full bg-black border-b border-gray-800 text-white flex items py-4  px-[15%] max-xl:px-[10%] max-md:px-[8%] max-sm:px-[4%] justify-between select-none">
+    <div className="fixed w-full bg-black border-b border-gray-800 text-white flex items py-4  px-[15%] max-xl:px-[10%] max-md:px-[8%] max-sm:px-[4%] justify-between select-none">
       <span className="font-bungee text-3xl duration-200 transition-all ease-in-out hover:text-gray-300 cursor-pointer">
         AVA LINK
       </span>
--- src/components/custom/SideBarNavigation.tsx
+++ src/components/custom/SideBarNavigation.tsx
@@ -3,18 +3,30 @@
 import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
+import { MenuData, MenuType, MenuLink } from "@/data/MenuItems";
 
 export default function SideBarNavigation() {
   const [showSide, setShowSide] = useState(false);
 
   return (
-    <button
-      className="hidden  max-lg:flex items-center justify-center active:scale-[.98] cursor-pointer duration-200 ease-in-out transition-all"
-      onClick={() => {
-        setShowSide(!showSide);
-      }}
-    >
-      {showSide ? <MdClose size={20} /> : <GiHamburgerMenu size={20} />}
-    </button>
+    <div className="flex justify-center items-center">
+      <button
+        className="hidden  max-lg:flex items-center justify-center active:scale-[.98] cursor-pointer duration-200 ease-in-out transition-all"
+        onClick={() => {
+          setShowSide(!showSide);
+        }}
+      >
+        {showSide ? <MdClose size={20} /> : <GiHamburgerMenu size={20} />}
+      </button>
+      {showSide ? (
+        <div className="absolute w-screen h-screen left-0 top-18 px-[15%] max-xl:px-[10%] max-md:px-[8%] max-sm:px-[4%] hidden max-lg:flex flex-col bg-black backdrop-blur-3xl font-geist">
+          {MenuData.map((menuItem: MenuType, index: number) => (
+            <h1>{menuItem.menuTrigger}</h1>
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
