73061b618dabf644b22dca00278dc90112394c3a
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-16 11:07:19 +0330

    add : closing an opening sidebar added

--- src/components/custom/SideBarNavigation.tsx
+++ src/components/custom/SideBarNavigation.tsx
@@ -1,13 +1,20 @@
+"use client";
+
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdClose } from "react-icons/md";
 
 export default function SideBarNavigation() {
+  const [showSide, setShowSide] = useState(false);
+
   return (
-    <div>
-      <button className="hidden  max-lg:flex items-center justify-center active:scale-[.98] cursor-pointer duration-200 ease-in-out transition-all ">
-        <GiHamburgerMenu size={20} />
-        <MdClose />
-      </button>
-    </div>
+    <button
+      className="hidden  max-lg:flex items-center justify-center active:scale-[.98] cursor-pointer duration-200 ease-in-out transition-all"
+      onClick={() => {
+        setShowSide(!showSide);
+      }}
+    >
+      {showSide ? <MdClose size={20} /> : <GiHamburgerMenu size={20} />}
+    </button>
   );
 }
