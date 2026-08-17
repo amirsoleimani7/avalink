d3d7a58c06550cd994285b3d9deae37954fdc50f
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-17 14:01:30 +0330

    add : open and close functioanlity added

--- src/components/custom/faq/FaqItem.tsx
+++ src/components/custom/faq/FaqItem.tsx
@@ -1,16 +1,25 @@
+"use client"
+import { useState } from "react";
 import { FiPlus } from "react-icons/fi";
 
 export default function FaqItem() {
+  const [show, setShow] = useState<boolean>(false);
+
   return (
-    <div className="p-5 rounded-xl border bg-main-bg">
-      <div className="flex items-center justify-between">
-        <h1 className="font-geist text-2xl">why use linktree</h1>
+    <div className="p-5 mt-5 rounded-xl border bg-main-bg">
+      <div
+        className="flex items-center justify-between select-none cursor-pointer"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        <h1 className="font-geist text-xl">why use linktree</h1>
         <button>
-          <FiPlus />
+          <FiPlus size={20} className={`${show ? "rotate-45" : "  "} duration-200 transition-all ease-in-out`}/>
         </button>{" "}
       </div>
 
-      <p className="font-geist text-main-secondary">
+      <p className="font-geist text-main-secondary mt-5">
         It's true. Nearly everything is technically free online. But so is the
         ability to learn brain surgery on YouTube. And so is the content you pay
         tens of thousands of dollars to university for. The question isn't
