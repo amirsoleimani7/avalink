cbe1029d4711df0bbe9d30cc8f6e07098bee6949
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-18 12:42:34 +0330

    add : meta to the page added

--- src/app/templates/page.tsx
+++ src/app/templates/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Templates",
+  description: "A libray with different avalink temapltes",
+};
+
 export default function Templates() {
   return <div>temapltes</div>;
 }
