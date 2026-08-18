e7b687558523bb4535b476fef47691b1accf51a3
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-18 14:30:36 +0330

    add : loading page for tempalte

--- src/app/templates/loading.tsx
+++ src/app/templates/loading.tsx
@@ -1,4 +1,14 @@
+import { Spinner } from "@/components/ui/spinner";
+
 export default function Loading() {
-  // Or a custom loading skeleton component
-  return <p className="mt-17">Loading...</p>
+  return (
+    <div className="flex w-full h-full mt-17 items-center justify-center bg-black *:text-white">
+      <div className=" font-semibold flex gap-5 items-center  justify-between bg-main-bg py-2 px-4 rounded-xl group cursor-pointer active:scale-[.98]">
+        <Spinner className="size-4 text-main-secondary duration-200 ease-in-out transition-all group-hover:text-white" />
+        <h1 className="font-geist text-main-secondary duration-200 ease-in-out transition-all group-hover:text-white">
+          Loading...
+        </h1>
+      </div>
+    </div>
+  );
 }
