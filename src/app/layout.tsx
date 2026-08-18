d3ae45d6857f1464c56dd67ef2ab2cf54b6a34da
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-18 12:42:06 +0330

    add : navigation and footer to layout so they are tempated into any apge

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Navigation from "@/components/custom/navigation/Navigation";
+import Footer from "@/components/custom/footer/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +29,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="w-full h-screen">{children}</body>
+      <body className="w-full h-screen">
+        <Navigation />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
