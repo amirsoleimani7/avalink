c5035ac86d2882dc3422bebba702f1dafdfc1237
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-21 19:44:41 +0330

    fix : main loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,12 +2,12 @@ import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
   return (
-    <div className="flex w-full h-full items-center justify-center bg-black *:text-white">
-      <div className="text-white font-semibold flex gap-1 text-center justify-center">
-        <h1 className="font-bold font-bungee text-6xl max-md:text-5xl max-sm  text-white">
-          ava Link
+    <div className="flex w-full h-full mt-17 items-center justify-center bg-black *:text-white">
+      <div className=" font-semibold flex gap-5 items-center  justify-between bg-main-bg py-2 px-4 rounded-xl group cursor-pointer active:scale-[.98]">
+        <Spinner className="size-4 text-main-secondary duration-200 ease-in-out transition-all group-hover:text-white" />
+        <h1 className="font-geist text-main-secondary duration-200 ease-in-out transition-all group-hover:text-white">
+          Loading...
         </h1>
-        <Spinner className="size-3" />
       </div>
     </div>
   );
