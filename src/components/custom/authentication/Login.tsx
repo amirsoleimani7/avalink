89ae34f43a209546df41a4555178f5b42a9790c4
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-21 17:20:30 +0330

    add : close functionality added

--- src/components/custom/authentication/Login.tsx
+++ src/components/custom/authentication/Login.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { MdClose } from "react-icons/md";
+
 import { motion, AnimatePresence } from "framer-motion";
 import { useAppSelector, useAppDispatch, useAppStore } from "@/lib/hooks";
 import { showLogin, hideLogin } from "@/lib/features/LoginForm/LoginShowSlice";
@@ -33,7 +35,7 @@ export default function Login() {
   return (
     <AnimatePresence>
       {ShowLoginSec && (
-        <div className="fixed inset-0 w-full h-full backdrop-blur-sm flex items-center justify-center  z-20">
+        <div className="fixed inset-0 w-full h-full backdrop-blur-sm flex items-center justify-center z-20">
           <motion.div
             initial={{ opacity: 0, filter: "blur(2px)", y: 10 }}
             animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
@@ -42,8 +44,19 @@ export default function Login() {
               duration: 0.1,
             }}
             ref={LoginDiv}
-            className=" rounded-xl top-1/2 left-1/2  w-100 h-100 bg-main-bg-1"
-          ></motion.div>
+            className="flex flex-col rounded-xl top-1/2 left-1/2  w-150 h-[50%] bg-main-bg-1 max-lg:w-full max-lg:mt-auto  p-5 border "
+          >
+            <div className="w-full flex justify-end">
+              <button
+                className="border rounded-full p-1.5 bg-main-hover duration-200 ease-in-out transition-all hover:bg-main-bg-1 cursor-pointer"
+                onClick={() => {
+                  dispatch(hideLogin());
+                }}
+              >
+                <MdClose size={20} />
+              </button>
+            </div>
+          </motion.div>
         </div>
       )}
     </AnimatePresence>
