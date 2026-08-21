3ac7b921c0c43a9f742e51c70c37bd4eb62dbf23
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-21 19:15:04 +0330

    add : refactor

--- src/components/custom/authentication/Login.tsx
+++ src/components/custom/authentication/Login.tsx
@@ -6,8 +6,8 @@ import { useAppDispatch } from "@/lib/hooks";
 export default function Login() {
   const dispatch = useAppDispatch();
   return (
-    <div className="flex flex-col ">
-      <div className="w-full flex justify-end">
+    <div className="flex flex-col gap-2">
+      <div className="w-full flex justify-end ">
         <button
           className="border rounded-full p-1.5 bg-main-hover duration-200 ease-in-out transition-all hover:bg-main-bg-1 cursor-pointer"
           onClick={() => {
@@ -21,13 +21,13 @@ export default function Login() {
       <p className="font-geist  text-sm text-main-secondary">
         Enter your email below to login to your account
       </p>
-      <div className="font-geist flex flex-col">
+      <div className="font-geist flex flex-col ">
         <label htmlFor="email-input text-sm ">Email</label>
         <input
           type="email"
           id="email-input"
           placeholder="m@example.com"
-          className="border rounded-lg mt-2 py-1 px-2 bg-main-bg-2 duration-200 ease-in-out transition-all"
+          className="border rounded-lg mt-1 py-2 px-2 bg-main-bg-2 duration-200 ease-in-out transition-all"
         />
       </div>
       <div className="font-geist flex flex-col">
@@ -38,18 +38,20 @@ export default function Login() {
         <input
           type="password"
           id="password-input"
-          className="border rounded-lg mt-2 py-1 px-2 bg-main-bg-2 duration-200 ease-in-out transition-all"
+          className="border rounded-lg mt-1 py-2 px-2 bg-main-bg-2 duration-200 ease-in-out transition-all"
         />
       </div>
-      <button className=" h-full bg-white px-2 py-1 rounded-lg duration-200 ease-out cursor-pointer  hover:inset-shadow-md hover:bg-gray-200 active:scale-[.98] ">
+      <button className="h-full bg-white px-2 py-1 rounded-lg duration-200 ease-out cursor-pointer  hover:inset-shadow-md hover:bg-gray-200 active:scale-[.98] mb-1">
         <span className="text-sm text-black font-geist">Login</span>
       </button>
-      <button className=" h-full  border-none outline outline-gray-800 px-2 py-1 rounded-lg duration-200 ease-out cursor-pointer  hover:inset-shadow-md hover:bg-black/70 hover:outline-gray-700 active:scale-[.98]">
+      <button className=" h-full bg-main-bg-2   border-none outline outline-gray-800 px-2 py-1 rounded-lg duration-200 ease-out cursor-pointer  hover:inset-shadow-md hover:bg-main-bg-1 hover:outline-gray-700 active:scale-[.98]">
         <span className="text-sm text-white font-geist">Login with Google</span>
       </button>
       <p className="font-geist text-center text-sm text-main-secondary">
         Don't have an account?{" "}
-        <span className="underline text-main-secondary duration-200 ease-in-out transition-all hover:text-white cursor-pointer">Sign up</span>
+        <span className="underline text-main-secondary duration-200 ease-in-out transition-all hover:text-white cursor-pointer">
+          Sign up
+        </span>
       </p>
     </div>
   );
