309d70cfa8636060c6141203e4aa092c2cc5eb95
amirsoleimani7 <a.soleimani@ec.iut.ac.ir>
2026-08-22 15:37:53 +0330

    add : fixed lag on drag

--- src/components/custom/authentication/mainAuth.tsx
+++ src/components/custom/authentication/mainAuth.tsx
@@ -8,15 +8,19 @@ import {
   useMotionValue,
 } from "framer-motion";
 import { useAppSelector, useAppDispatch, useAppStore } from "@/lib/hooks";
-import { hideAll } from "@/lib/features/LoginForm/LoginShowSlice";
-import { useEffect, useRef, useState } from "react";
+import {
+  hideAll,
+  showLogin,
+  showSignUp,
+} from "@/lib/features/LoginForm/LoginShowSlice";
+import { Children, useEffect, useRef, useState } from "react";
 import Login from "./Login";
 import SignIn from "./SignIn";
 import Forgot from "./Forgot";
+import { showSide } from "@/lib/features/Sidebar/SidebarSlice";
 
 export default function MainAuth() {
-
-  const [scope , animate] = useAnimate();
+  const [scope, animate] = useAnimate();
   const controls = useDragControls();
   const y = useMotionValue(0);
 
@@ -52,11 +56,13 @@ export default function MainAuth() {
       {ShowAuth && (
         <div className="fixed inset-0 w-full h-full backdrop-blur-sm flex items-center justify-center z-20 ">
           <motion.div
-            style={{ y }}
+            style={{
+              transition: "height 0.1s ease",
+              y,
+            }}
             initial={{ opacity: 0, filter: "blur(2px)", y: 10 }}
             animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
             exit={{ opacity: 0, filter: "blur(2px)", y: -10 }}
-            
             transition={{
               duration: 0.1,
               type: "tween",
@@ -71,6 +77,7 @@ export default function MainAuth() {
             drag="y"
             dragControls={controls}
             dragListener={false}
+            dragMomentum={false}
             dragConstraints={{
               top: 0,
               bottom: 0,
@@ -80,9 +87,9 @@ export default function MainAuth() {
               bottom: 0.5,
             }}
             ref={authDiv}
-            className="flex flex-col rounded-xl top-1/2 left-1/2  w-100  bg-main-bg max-sm:w-full max-sm:mt-auto max-sm:rounded-b-none  p-5 pt-0 border duration-200 ease-in-out transition-all"
+            className="flex flex-col rounded-xl top-1/2 left-1/2  w-100  bg-main-bg max-sm:w-full max-sm:mt-auto max-sm:rounded-b-none p-5 pt-0  border duration-200 ease-in-out transition-all"
           >
-            <div className="hidden max-sm:flex  justify-center">
+            <div className="hidden max-sm:flex  justify-center ">
               <button
                 className="cursor-grab active:cursor-grabbing touch-none  w-1/5 h-2  mx-auto rounded-xl mt-2 hover:w-1/4 duration-200 ease-in-out transition-all  bg-main-bg-1 hover:bg-main-secondary hover:scale-110"
                 onPointerDown={(e) => {
